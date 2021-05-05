06e65da121c0b9c30492a6bfe2640252d694113d
Christina Chang <chlchang@ucdavis.edu>
2021-05-05 08:59:59 -0500

    Modify column types.

--- src/create_tables.py
+++ src/create_tables.py
@@ -18,23 +18,23 @@ class Trails(Base):
     __tablename__ = 'trails'
 
     trail_id = Column(Integer, primary_key=True)
-    name = Column(String(100), unique=False, nullable=False)
-    area_name = Column(String(100), unique=False, nullable=False)
-    city_name = Column(String(100), unique=False, nullable=False)
-    state_name = Column(String(100), unique=False, nullable=False)
-    country_name = Column(String(100), unique=False, nullable=False)
-    _geoloc = Column(String(100), unique=False, nullable=False)
+    name = Column(String(50), unique=False, nullable=False)
+    area_name = Column(String(50), unique=False, nullable=False)
+    city_name = Column(String(50), unique=False, nullable=False)
+    state_name = Column(String(20), unique=False, nullable=False)
+    country_name = Column(String(20), unique=False, nullable=False)
+    _geoloc = Column(String(50), unique=False, nullable=False)
     popularity = Column(Float, unique=False, nullable=False)
     length = Column(Float, unique=False, nullable=False)
     elevation_gain = Column(Float, unique=False, nullable=False)
     difficulty_rating = Column(Integer, unique=False, nullable=False)
-    route_type = Column(String(100), unique=False, nullable=False)
+    route_type = Column(String(20), unique=False, nullable=False)
     visitor_usage = Column(Integer, unique=False, nullable=False)
     avg_rating = Column(Float, unique=False, nullable=False)
     num_reviews = Column(Integer, unique=False, nullable=False)
     features = Column(String(100), unique=False, nullable=False)
     activities = Column(String(100), unique=False, nullable=False)
-    units = Column(String(100), unique=False, nullable=False)
+    units = Column(String(10), unique=False, nullable=False)
 
     def __repr__(self):
         return '<Trail: %r>' % self.name
