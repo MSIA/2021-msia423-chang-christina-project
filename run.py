54b7e3dfd176a067b188fd3c090679542cf052a6
Christina Chang <chlchang@ucdavis.edu>
2021-05-06 00:59:39 -0500

    Parsers for S3 upload and create database

--- run.py
+++ run.py
@@ -4,16 +4,15 @@ import logging.config
 logging.config.fileConfig('config/logging/local.conf')
 #logger = logging.getLogger('penny-lane-pipeline')
 
-#from src.add_songs import TrackManager, create_db
 from src.s3 import upload_file_to_s3
-from src.create_tables import create_db
+from src.create_mysql import HikeManager, create_db
 from config.flaskconfig import SQLALCHEMY_DATABASE_URI
 
 if __name__ == '__main__':
 
     # Add parsers for both creating a database
     parser = argparse.ArgumentParser(description="Create database")
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest="subparser_name")
 
     # Sub-parser for uploading data to S3 bucket
     sb_s3 = subparsers.add_parser("s3_upload", description="Upload data to S3")
@@ -27,14 +26,10 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     sp_used = args.subparser_name
-    if sp_used == 'create_db':
-        create_db(args.engine_string)
-    elif sp_used == 's3_upload':
+    if sp_used == 's3_upload':
         upload_file_to_s3(args.local_path, args.s3path)
-    # elif sp_used == 'ingest':
-    #     tm = TrackManager(engine_string=args.engine_string)
-    #     tm.add_track(args.title, args.artist, args.album)
-    #     tm.close()
+    elif sp_used == 'create_db':
+        create_db(args.engine_string)
     else:
         parser.print_help()
 
