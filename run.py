527d3e845ad5ce5c3d2678a2730b4fdbc9e383a6
Christina Chang <chlchang@ucdavis.edu>
2021-05-10 15:21:16 -0500

    Update default value for s3_upload local_path

--- run.py
+++ run.py
@@ -11,20 +11,20 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
 
     # Create parser
-    parser = argparse.ArgumentParser(description="Create database or upload data to S3")
-    subparsers = parser.add_subparsers(dest="subparser_name")
+    parser = argparse.ArgumentParser(description='Create database or upload data to S3')
+    subparsers = parser.add_subparsers(dest='subparser_name')
 
     # Sub-parser for uploading data to S3 bucket
-    sb_s3 = subparsers.add_parser("s3_upload", description="Upload data to S3")
+    sb_s3 = subparsers.add_parser('s3_upload', description='Upload data to S3')
     sb_s3.add_argument('--s3path', default='s3://2021-msia423-chang-christina/raw/national-park-trails.csv',
-                       help="Where to load data to in S3")
-    sb_s3.add_argument('--local_path', default='data/external/national-park-trails.csv',
-                       help="Where data exists in local")
+                       help='Where to load data to in S3')
+    sb_s3.add_argument('--local_path', default='./data/external/national-park-trails.csv',
+                       help='Where data exists in local')
 
     # Sub-parser for creating a database
-    sb_create = subparsers.add_parser("create_db", description="Create database")
-    sb_create.add_argument("--engine_string", default=SQLALCHEMY_DATABASE_URI,
-                           help="SQLAlchemy connection URI for database")
+    sb_create = subparsers.add_parser('create_db', description='Create database')
+    sb_create.add_argument('--engine_string', default=SQLALCHEMY_DATABASE_URI,
+                           help='SQLAlchemy connection URI for database')
 
     args = parser.parse_args()
     sp_used = args.subparser_name
