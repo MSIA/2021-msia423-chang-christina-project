ce16ff4993b12d3845fab3c8eb0c2473f99f9d80
Christina Chang <chlchang@ucdavis.edu>
2021-05-31 15:59:01 -0500

    Integrate s3 download

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import logging.config
 
 from config.flaskconfig import SQLALCHEMY_DATABASE_URI
 from src.create_db import create_db
-from src.s3 import upload_file_to_s3
+from src.s3 import upload_file_to_s3, download_file_from_s3
 
 logging.config.fileConfig('config/logging/local.conf', disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
@@ -15,10 +15,15 @@ if __name__ == '__main__':
     subparsers = parser.add_subparsers(dest='subparser_name')
 
     # Sub-parser for uploading data to S3 bucket
-    sb_s3 = subparsers.add_parser('s3_upload', description='Upload data to S3')
-    sb_s3.add_argument('--s3path', required=True, help='Where to load data to in S3')
-    sb_s3.add_argument('--local_path', default='./data/raw/national-park-trails.csv',
-                       help='Where data exists in local')
+    sb_s3_upload = subparsers.add_parser('s3_upload', description='Upload data to S3')
+    sb_s3_upload.add_argument('--s3path', required=True, help='Where to load data to in S3')
+    sb_s3_upload.add_argument('--local_path', default='./data/raw/national-park-trails.csv',
+                              help='Where data exists in local')
+
+    # sub-parser for download data to S3 bucket
+    sb_s3_download = subparsers.add_parser('s3_download', description='Download data from S3')
+    sb_s3_download.add_argument('--local_path', required=True, help='Where to download in local')
+    sb_s3_download.add_argument('--s3path', required=True, help='Path to S3')
 
     # Sub-parser for creating a database
     sb_create = subparsers.add_parser('create_db', description='Create database')
@@ -29,6 +34,8 @@ if __name__ == '__main__':
     sp_used = args.subparser_name
     if sp_used == 's3_upload':
         upload_file_to_s3(args.local_path, args.s3path)
+    elif sp_used == 's3_download':
+        download_file_from_s3(args.local_path, args.s3path)
     elif sp_used == 'create_db':
         create_db(args.engine_string)
     else:
