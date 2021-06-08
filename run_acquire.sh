988b4d8e192fafa3171d74bf96c54685432306df
Christina Chang <chlchang@ucdavis.edu>
2021-06-08 12:08:45 -0500

    Set up Dockerfile for acquire, pipeline, and app

---
+++ run_acquire.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+python3 run.py s3_upload \
+  --s3path s3://2021-msia423-chang-christina/raw/national-park-trails.csv \
+  --local_path ./data/raw/national-park-trails.csv
+python3 run.py s3_download \
+  --s3path s3://2021-msia423-chang-christina/raw/national-park-trails.csv \
+  --local_path ./data/raw/national-park-trails.csv
+python3 run.py create_db
