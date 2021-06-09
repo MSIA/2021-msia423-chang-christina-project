2c7950a023bfdb8c8ddfb069cb701e2e9ac8db10
Christina Chang <chlchang@ucdavis.edu>
2021-06-09 00:33:52 -0500

    Move s3 download to pipeline shell script

--- run_acquire.sh
+++ run_acquire.sh
@@ -3,7 +3,4 @@
 python3 run.py s3_upload \
   --s3path s3://2021-msia423-chang-christina/raw/national-park-trails.csv \
   --local_path ./data/raw/national-park-trails.csv
-python3 run.py s3_download \
-  --s3path s3://2021-msia423-chang-christina/raw/national-park-trails.csv \
-  --local_path ./data/raw/national-park-trails.csv
 python3 run.py create_db
--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+python3 run.py s3_download \
+  --s3path s3://2021-msia423-chang-christina/raw/national-park-trails.csv \
+  --local_path ./data/raw/national-park-trails.csv
 python3 run.py clean
 python3 run.py featurize
 python3 run.py model
