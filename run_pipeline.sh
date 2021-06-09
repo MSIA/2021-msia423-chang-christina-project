033820d50ddff40fe8324806711c3fdfd65e4c82
Christina Chang <chlchang@ucdavis.edu>
2021-06-08 23:06:14 -0500

    Clean up shell script for running pipeline

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-#python3 run.py s3_upload --s3path s3://2021-msia423-chang-christina/raw/national-park-trails.csv \
-#  --local_path ./data/raw/national-park-trails.csv
-#python3 run.py s3_download --s3path s3://2021-msia423-chang-christina/raw/national-park-trails.csv \
-#  --local_path ./data/raw/national-park-trails.csv
 python3 run.py clean
 python3 run.py featurize
 python3 run.py model
-# python3 app.py
